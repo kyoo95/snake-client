7ad38020b9c81958df3cac6b253311fcbfae10f9
Kevin <kevinyoo0203@yahoo.com>
2019-07-31 18:57:04 +0000

    added ctrl c exit

--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+const handUserInput = function(key) {
+    //ctrl + c exit implementation
+    if (key === '\u0003') {
+      console.log("Exiting")
+      process.exit();
+    }
+  }
+  
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (handUserInput));
+  return stdin;
+}
+setupInput();
+
