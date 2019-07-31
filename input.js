5da745538e66d3feab0aa4204ab755c4768b7302
Kevin <kevinyoo0203@yahoo.com>
2019-07-31 19:17:46 +0000

    fixed controls

--- input.js
+++ input.js
@@ -1,13 +1,29 @@
+let connection;
+
+
 const handUserInput = function(key) {
   //ctrl + c exit implementation
   if (key === '\u0003') {
     console.log("Exiting")
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
+  }
 }
 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+connection = conn;
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
