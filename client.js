218424a60672e33a152b868437cf7c457c93fe7b
Kevin <kevinyoo0203@yahoo.com>
2019-07-31 20:12:14 +0000

    IP and Port edited to be static/constants

--- client.js
+++ client.js
@@ -1,18 +1,20 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '172.46.0.110',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: LOL");
+    // conn.write("Move: up");
+    
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
